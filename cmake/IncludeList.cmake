09faaf05305da5a47f262c43cf8c2059576b93be
Hari Shankar <harishankarn04@gmail.com>
2025-06-30 15:03:20 +0530

    cmake

---
+++ cmake/IncludeList.cmake
@@ -0,0 +1,10 @@
+set(include_list ${include_list}
+${PROJ_PATH}/Application/Inc
+${PROJ_PATH}/Application/Inc
+${PROJ_PATH}/Core/Inc
+${PROJ_PATH}/Drivers/CMSIS/Include
+${PROJ_PATH}/Drivers/CMSIS/STM32F4xx_HAL_Driver/Inc
+${PROJ_PATH}/Drivers/CMSIS/STM32F4xx_HAL_Driver/Inc/Legacy
+${PROJ_PATH}/Drivers/CMSIS/STM32F4xx_HAL_Driver/Src/Legacy
+${PROJ_PATH}/Drivers/CMSIS/Device/ST/STM32F4xx/Include
+)
