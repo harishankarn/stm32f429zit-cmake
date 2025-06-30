6010cfa7b9a2f36c6e0dc68f20517417781f5910
Hari Shankar <harishankarn04@gmail.com>
2025-06-30 15:12:33 +0530

    Application source files

---
+++ Application/Src/main.c
@@ -0,0 +1,34 @@
+/*	Find where USB B connection is connected with board via UART/USART
+ *	Board : STM32F429ZI
+ * 	This USB communicates via Virtual COMM Port through USART1
+ * 	USART1 address : 0x4001 1000 - 0x4001 13FF
+ * 	TX_Port: Port A
+ * 	TX_Pin: 9
+ *
+ *	RX_Port: Port A
+ *	RX_Pin: 10
+ *
+ *	Address Bus : APB2 90 MHZ @ USART1
+ *
+ * 	USART1 uses GPIOA : PA09(TX) PA10(RX)
+ * 	So,
+ * 		- need to map with alternate function mapping in Data sheet
+ * 		- find type of alternate function (AF7)
+ */
+
+
+#include <stdio.h>		// Standard Input/Output library
+#include "UART.h"
+
+
+int main(void){
+	usart1_tx_init();
+
+	while(1)
+	{
+		printf("/********Checking text printing********/\n\r");
+		for(volatile int i = 0; i < 100000; i++);
+	}
+}
+
+
