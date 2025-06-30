64017141da64389c7b1294014e41e3f5f1c7e5b1
Hari Shankar <harishankarn04@gmail.com>
2025-06-30 15:05:23 +0530

    Core include files

---
+++ Core/Inc/UART.h
@@ -0,0 +1,10 @@
+#ifndef UART_H_
+#define UART_H_
+
+#include "stm32f4xx.h"	// STM32F4 library
+#include <stdint.h>		// Standard C library
+#include <unistd.h>		// POSIX (Unix standard) header
+void usart1_tx_init(void);
+
+
+#endif /* UART_H_ */
