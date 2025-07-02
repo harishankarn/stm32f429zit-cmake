683401c33bd8ede6a6cb31a0ef51d5e6d13b57b0
Hari Shankar <harishankarn04@gmail.com>
2025-07-02 23:43:13 +0530

    changes to cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-rm -r ./build ./CMakeCache.txt
+
+# Remove build directory
+if [ -d "./build" ]; then
+    rm -r ./build
+    echo "Removed ./build"
+else
+    echo "./build has already been removed"
+fi
+
+# Remove CMakeCache.txt
+if [ -f "./CMakeCache.txt" ]; then
+    rm ./CMakeCache.txt
+    echo "Removed ./CMakeCache.txt"
+else
+    echo "./CMakeCache.txt has already been removed"
+fi
+
