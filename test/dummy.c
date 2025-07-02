336e2ce4e18b7867c54b77d98aef0e457a383912
Hari Shankar <harishankarn04@gmail.com>
2025-07-02 23:44:01 +0530

    test case files

---
+++ test/dummy.c
@@ -0,0 +1,11 @@
+#include "./Core/inc/ADC.h"
+
+
+// Tests that don't naturally fit in the headers/.cpp files directly
+// can be placed in a tests/*.cpp file. Integration tests are a good example.
+
+TEST_CASE("Test case for ADC functionality")
+{
+  Dummy d;
+  CHECK(d.doSomething() == true);
+}
