e6d0de4c45641d40ca0d98baf0c4bcd05a314182
Hari Shankar N <harishankarn04@gmail.com>
2025-07-03 16:11:12 +0530

    updated added

---
+++ Tests/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /opt/software/stm32f429zit-cmake/Tests/_deps/googletest-src
+# Build directory: /opt/software/stm32f429zit-cmake/Tests/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ Tests/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /opt/software/stm32f429zit-cmake/Tests/_deps/googletest-src/googletest
+# Build directory: /opt/software/stm32f429zit-cmake/Tests/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
