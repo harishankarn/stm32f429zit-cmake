d8b256858316cfa079b18eaecc66c33146b74f27
Hari Shankar N <harishankarn04@gmail.com>
2025-07-03 15:40:36 +0530

    updated added

--- cmake/IncludeList.cmake
+++ cmake/IncludeList.cmake
@@ -3,5 +3,6 @@ ${PROJ_PATH}/Application/Inc
 ${PROJ_PATH}/Application/Inc
 ${PROJ_PATH}/Core/Inc
 ${PROJ_PATH}/Drivers/CMSIS/Include
-${PROJ_PATH}/Drivers/CMSIS/Device/ST/STM32F4xx/Include)
-
+${PROJ_PATH}/Drivers/CMSIS/Device/ST/STM32F4xx/Include
+)
+)
