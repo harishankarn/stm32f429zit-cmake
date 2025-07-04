4ac212ba8711d45ecabc43c673eab4655e666bd7
Hari Shankar <harishankarn04@gmail.com>
2025-07-04 16:44:17 +0530

    Update flash script

--- flash.sh
+++ flash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+PROJECT_NAME="stm32f429zit-cmake"
+
 openocd -f interface/stlink.cfg -f target/stm32f4x.cfg \
   -c "init" \
   -c "reset halt" \
-  -c "program build/STM32-cmake-tmp-trial.elf verify reset exit"
+  -c "program build/$PROJECT_NAME.elf verify reset exit"
