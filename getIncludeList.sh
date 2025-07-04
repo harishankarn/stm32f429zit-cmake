bef9ff41d0d68bc219693714d276fae084f0b1cf
Hari Shankar <harishankarn04@gmail.com>
2025-07-04 17:06:22 +0530

    updated script

--- getIncludeList.sh
+++ getIncludeList.sh
@@ -65,11 +65,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
 	# Struct cmake format (Do not add tab space to line - 68)
 	sed -i '' '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' cmake/IncludeList.cmake
-
-	# Only add closing ')' if not already present
-	if [[ "$(tail -n 1 cmake/IncludeList.cmake)" != ")" ]]; then
-    	echo ")" >> cmake/IncludeList.cmake
-	fi
 else
 	# ========== FOR LINUX  ==========
 	# Remove Empty Line
@@ -80,9 +75,9 @@ else
 
 	# Struct cmake format
 	sed -i '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' cmake/IncludeList.cmake
+fi
 
-	# Only add closing ')' if not already present
-	if [[ "$(tail -n 1 cmake/IncludeList.cmake)" != ")" ]]; then
-		echo ")" >> cmake/IncludeList.cmake
-	fi
+# Only add closing ')' if not already present
+if [[ "$(tail -n 1 cmake/IncludeList.cmake)" != ")" ]]; then
+    echo ")" >> cmake/IncludeList.cmake
 fi
