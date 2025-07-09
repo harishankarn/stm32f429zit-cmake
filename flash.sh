d933046b9d4f8510908ff7832df5974a254ef5ba
Hari Shankar <harishankarn04@gmail.com>
2025-07-09 18:12:53 +0530

    flash script update

--- flash.sh
+++ flash.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
+
 # Load project name from config
 source ./project.conf
 
-openocd -f interface/stlink.cfg -f target/stm32f4x.cfg \
-  -c "init" \
-  -c "reset halt" \
-  -c "program build/$PROJECT_NAME.elf verify reset exit"
+openocd -f interface/stlink.cfg \
+        -f target/stm32f4x.cfg \
+        -c "init" \
+        -c "reset halt" \
+        -c "program build/$PROJECT_NAME.elf verify reset exit"
