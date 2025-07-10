6e1b2cbc3e84d92c819f9267e1d311dc601d2778
Hari Shankar <harishankarn04@gmail.com>
2025-07-10 10:55:08 +0530

    getList update

--- getIncludeList.sh
+++ getIncludeList.sh
@@ -11,75 +11,75 @@ DRIVERS_PATH='Drivers'
 # RTOS_PATH='Middlewares/Third_Party/FreeRTOS/*'
 # USB_PATH='USB_Device/*'
 
-CMAKE_FILE_PATH=cmake/IncludeList.cmake
+CMAKE_INC_FILE_PATH=cmake/IncludeList.cmake
 
 # Clear the file
-> "$CMAKE_FILE_PATH"
+> "$CMAKE_INC_FILE_PATH"
 
 if [ -n "$APP_PATH" ]; then
 	APP_INC="$(find "$APP_PATH" -type d -name 'Inc')"
-	printf "$APP_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$APP_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 if [ -n "$DEP_LIB_PATH" ]; then
 	DEP_LIB_INC="$(find "$DEP_LIB_PATH" -type d)"
-	printf "$DEP_LIB_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$DEP_LIB_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 if [ -n "$CORE_PATH" ]; then
 	CORE_INC="$(find "$CORE_PATH" -type d -name 'Inc' -o -name 'Include')"
-	printf "$CORE_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$CORE_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 if [ -n "$DRIVERS_PATH" ]; then
 	DRIVERS_INC="$(find "$DRIVERS_PATH" -type d -name 'Inc' -o -name 'Include' -o -name 'Legacy')"
-	printf "$DRIVERS_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$DRIVERS_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 if [ -n "$FATFS_PATH" ]; then
 	FATFS_INC="$(find "$FATFS_PATH" -type d)"
-	printf "$FATFS_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$FATFS_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 if [ -n "$RTOS_PATH" ]; then
 	RTOS_INC="$(find "$MIDWARE_PATH" -type d -name 'include' -o -name 'CMSIS_RTOS' -o -name 'ARM_CM4F')"
-	printf "$RTOS_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$RTOS_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 if [ -n "$MIDWARE_PATH" ]; then
 	MIDWARE_INC="$(find "$MIDWARE_PATH" -type d -name 'Inc' -o -name 'src')"
-	printf "$MIDWARE_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$MIDWARE_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 if [ -n "$USB_PATH" ]; then
 	USB_INC="$(find "$USB_PATH" -type d)"
-	printf "$USB_INC\n" >>"$CMAKE_FILE_PATH"
+	printf "$USB_INC\n" >>"$CMAKE_INC_FILE_PATH"
 fi
 
 # sed command works different for MacOS and Linux 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	# ========== FOR MACOS ==========
 	# Remove Empty Line
-	sed -i '' '/./!d' "$CMAKE_FILE_PATH"
+	sed -i '' '/./!d' "$CMAKE_INC_FILE_PATH"
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i '' 's|^|${PROJ_PATH}/|' "$CMAKE_FILE_PATH"
+	sed -i '' 's|^|${PROJ_PATH}/|' "$CMAKE_INC_FILE_PATH"
 
 	# Struct cmake format (Do not add tab space to line - 68)
-	sed -i '' '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' "$CMAKE_FILE_PATH"
+	sed -i '' '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' "$CMAKE_INC_FILE_PATH"
 else
 	# ========== FOR LINUX  ==========
 	# Remove Empty Line
-	sed -i '/./!d' "$CMAKE_FILE_PATH"
+	sed -i '/./!d' "$CMAKE_INC_FILE_PATH"
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i 's|^|${PROJ_PATH}/|' "$CMAKE_FILE_PATH"
+	sed -i 's|^|${PROJ_PATH}/|' "$CMAKE_INC_FILE_PATH"
 
 	# Struct cmake format
-	sed -i '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' "$CMAKE_FILE_PATH"
+	sed -i '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' "$CMAKE_INC_FILE_PATH"
 fi
 
 # Only add closing ')' if not already present
-if [[ "$(tail -n 1 "$CMAKE_FILE_PATH")" != ")" ]]; then
-    echo ")" >> "$CMAKE_FILE_PATH"
+if [[ "$(tail -n 1 "$CMAKE_INC_FILE_PATH")" != ")" ]]; then
+    echo ")" >> "$CMAKE_INC_FILE_PATH"
 fi
--- getSourceList.sh
+++ getSourceList.sh
@@ -11,64 +11,64 @@ DRIVERS_PATH='Drivers/CMSIS/Device' # <----Changed path from Drivers/* which inc
 # MIDWARE_PATH='Middlewares/*'
 # USB_PATH='USB_Device/*'
 
-CMAKE_FILE_PATH=cmake/SourceList.cmake
+CMAKE_SRC_FILE_PATH=cmake/SourceList.cmake
 
 # Clear the file
-> "$CMAKE_FILE_PATH"
+> "$CMAKE_SRC_FILE_PATH"
 
 # Print find result
 if [ -n "$APP_PATH" ]; then
 	APP_SRC="$(find "$APP_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
-	printf "$APP_SRC\n" >> "$CMAKE_FILE_PATH"
+	printf "$APP_SRC\n" >> "$CMAKE_SRC_FILE_PATH"
 fi
 
 if [ -n "$CORE_PATH" ]; then
 	CORE_SRC="$(find "$CORE_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
-	printf "$CORE_SRC\n" >> "$CMAKE_FILE_PATH"
+	printf "$CORE_SRC\n" >> "$CMAKE_SRC_FILE_PATH"
 fi
 
 if [ -n "$DRIVERS_PATH" ]; then
 	DRIVERS_SRC="$(find "$DRIVERS_PATH" -type f -name '*.c')"
-	printf "$DRIVERS_SRC\n" >> "$CMAKE_FILE_PATH"
+	printf "$DRIVERS_SRC\n" >> "$CMAKE_SRC_FILE_PATH"
 fi
 
 if [ -n "$FATFS_PATH" ]; then
 	FATFS_SRC="$(find "$FATFS_PATH" -type f -name '*.c')"
-	printf "$FATFS_SRC\n" >> "$CMAKE_FILE_PATH"
+	printf "$FATFS_SRC\n" >> "$CMAKE_SRC_FILE_PATH"
 fi
 
 if [ -n "$MIDWARE_PATH" ]; then
 	MIDWARE_SRC="$(find "$MIDWARE_PATH" -type f -name '*.c')"
-	printf "$MIDWARE_SRC\n" >> "$CMAKE_FILE_PATH"
+	printf "$MIDWARE_SRC\n" >> "$CMAKE_SRC_FILE_PATH"
 fi
 
 if [ -n "$USB_PATH" ]; then
 	USB_SRC="$(find "$USB_PATH" -type f -name '*.c')"
-	printf "$USB_SRC\n" >> "$CMAKE_FILE_PATH"
+	printf "$USB_SRC\n" >> "$CMAKE_SRC_FILE_PATH"
 fi
 
 # sed command works different for MacOS and Linux 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	# ========== FOR MACOS ==========
 	# Remove Empty Line
-	sed -i '' '/./!d' "$CMAKE_FILE_PATH"
+	sed -i '' '/./!d' "$CMAKE_SRC_FILE_PATH"
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i '' 's|^|${PROJ_PATH}/|' "$CMAKE_FILE_PATH"
+	sed -i '' 's|^|${PROJ_PATH}/|' "$CMAKE_SRC_FILE_PATH"
 
 	# Struct cmake format (Do not add tab space to line - 58)
 	sed -i '' '1i\
-set(source_list ${source_list}' "$CMAKE_FILE_PATH"
-	sed -i '' -e '$a\'$'\n'')' "$CMAKE_FILE_PATH"
+set(source_list ${source_list}' "$CMAKE_SRC_FILE_PATH"
+	sed -i '' -e '$a\'$'\n'')' "$CMAKE_SRC_FILE_PATH"
 else
 	# ========== FOR LINUX  ==========
 	# Remove Empty Line
-	sed -i '/./!d' "$CMAKE_FILE_PATH"
+	sed -i '/./!d' "$CMAKE_SRC_FILE_PATH"
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i 's|^|${PROJ_PATH}/|' "$CMAKE_FILE_PATH"
+	sed -i 's|^|${PROJ_PATH}/|' "$CMAKE_SRC_FILE_PATH"
 
 	# Struct cmake format
-	sed -i '1i set(source_list ${source_list}' "$CMAKE_FILE_PATH"
-	sed -i '$a)' "$CMAKE_FILE_PATH"
+	sed -i '1i set(source_list ${source_list}' "$CMAKE_SRC_FILE_PATH"
+	sed -i '$a)' "$CMAKE_SRC_FILE_PATH"
 fi
