c59d36d35e9f39f3de26d57313ddc73126e586cb
Hari Shankar <harishankarn04@gmail.com>
2025-07-10 10:32:16 +0530

    cmakelist update

--- cmake/SourceList.cmake
+++ cmake/SourceList.cmake
@@ -1,12 +1,11 @@
-set(source_list ${source_list}
-${PROJ_PATH}/Application/Src/main.c
-${PROJ_PATH}/Core/Src/sysmem.c
-${PROJ_PATH}/Core/Src/ADC.c
-${PROJ_PATH}/Core/Src/systick.c
-${PROJ_PATH}/Core/Src/UART.c
-${PROJ_PATH}/Core/Src/timer.c
-${PROJ_PATH}/Core/Src/syscalls.c
+set(source_list ${source_list}${PROJ_PATH}/Application/Src/main.c
 ${PROJ_PATH}/Core/Src/LED.c
+${PROJ_PATH}/Core/Src/timer.c
+${PROJ_PATH}/Core/Src/systick.c
+${PROJ_PATH}/Core/Src/syscalls.c
+${PROJ_PATH}/Core/Src/ADC.c
+${PROJ_PATH}/Core/Src/sysmem.c
+${PROJ_PATH}/Core/Src/UART.c
 ${PROJ_PATH}/Core/startup_stm32f429zitx.s
 ${PROJ_PATH}/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
 )
--- getIncludeList.sh
+++ getIncludeList.sh
@@ -12,72 +12,72 @@ DRIVERS_PATH='Drivers'
 # USB_PATH='USB_Device/*'
 
 # Clear the file
-> cmake/IncludeList.cmake
+> ./cmake/IncludeList.cmake
 
 if [ -n "$APP_PATH" ]; then
 	APP_INC="$(eval find "$APP_PATH" -type d -name 'Inc')"
-	printf "$APP_INC\n" >>cmake/IncludeList.cmake
+	printf "$APP_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 if [ -n "$DEP_LIB_PATH" ]; then
 	DEP_LIB_INC="$(eval find "$DEP_LIB_PATH" -type d)"
-	printf "$DEP_LIB_INC\n" >>cmake/IncludeList.cmake
+	printf "$DEP_LIB_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 if [ -n "$CORE_PATH" ]; then
 	CORE_INC="$(eval find "$CORE_PATH" -type d -name 'Inc' -o -name 'Include')"
-	printf "$CORE_INC\n" >>cmake/IncludeList.cmake
+	printf "$CORE_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 if [ -n "$DRIVERS_PATH" ]; then
 	DRIVERS_INC="$(eval find "$DRIVERS_PATH" -type d -name 'Inc' -o -name 'Include' -o -name 'Legacy')"
-	printf "$DRIVERS_INC\n" >>cmake/IncludeList.cmake
+	printf "$DRIVERS_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 if [ -n "$FATFS_PATH" ]; then
 	FATFS_INC="$(eval find "$FATFS_PATH" -type d)"
-	printf "$FATFS_INC\n" >>cmake/IncludeList.cmake
+	printf "$FATFS_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 if [ -n "$RTOS_PATH" ]; then
 	RTOS_INC="$(eval find "$MIDWARE_PATH" -type d -name 'include' -o -name 'CMSIS_RTOS' -o -name 'ARM_CM4F')"
-	printf "$RTOS_INC\n" >>cmake/IncludeList.cmake
+	printf "$RTOS_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 if [ -n "$MIDWARE_PATH" ]; then
 	MIDWARE_INC="$(eval find "$MIDWARE_PATH" -type d -name 'Inc' -o -name 'src')"
-	printf "$MIDWARE_INC\n" >>cmake/IncludeList.cmake
+	printf "$MIDWARE_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 if [ -n "$USB_PATH" ]; then
 	USB_INC="$(eval find "$USB_PATH" -type d)"
-	printf "$USB_INC\n" >>cmake/IncludeList.cmake
+	printf "$USB_INC\n" >>./cmake/IncludeList.cmake
 fi
 
 # sed command works different for MacOS and Linux 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	# ========== FOR MACOS ==========
 	# Remove Empty Line
-	sed -i '' '/./!d' cmake/IncludeList.cmake
+	sed -i '' '/./!d' ./cmake/IncludeList.cmake
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i '' 's|^|${PROJ_PATH}/|' cmake/IncludeList.cmake
+	sed -i '' 's|^|${PROJ_PATH}/|' ./cmake/IncludeList.cmake
 
 	# Struct cmake format (Do not add tab space to line - 68)
-	sed -i '' '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' cmake/IncludeList.cmake
+	sed -i '' '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' ./cmake/IncludeList.cmake
 else
 	# ========== FOR LINUX  ==========
 	# Remove Empty Line
-	sed -i '/./!d' cmake/IncludeList.cmake
+	sed -i '/./!d' ./cmake/IncludeList.cmake
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i 's|^|${PROJ_PATH}/|' cmake/IncludeList.cmake
+	sed -i 's|^|${PROJ_PATH}/|' ./cmake/IncludeList.cmake
 
 	# Struct cmake format
-	sed -i '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' cmake/IncludeList.cmake
+	sed -i '1s|^|set(include_list ${include_list}\n|; $s|$|\n)|' ./cmake/IncludeList.cmake
 fi
 
 # Only add closing ')' if not already present
-if [[ "$(tail -n 1 cmake/IncludeList.cmake)" != ")" ]]; then
-    echo ")" >> cmake/IncludeList.cmake
+if [[ "$(tail -n 1 ./cmake/IncludeList.cmake)" != ")" ]]; then
+    echo ")" >> ./cmake/IncludeList.cmake
 fi
