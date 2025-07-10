b79fdd4276140e0ccb140bdc18d14d19bc147763
Hari Shankar <harishankarn04@gmail.com>
2025-07-10 10:34:44 +0530

    Revert "cmakelist update"
    This reverts commit c59d36d35e9f39f3de26d57313ddc73126e586cb.

--- cmake/STM32F429xx_HAL_PARA.cmake
+++ cmake/STM32F429xx_HAL_PARA.cmake
@@ -6,5 +6,6 @@ set(CPU_PARAMETERS ${CPU_PARAMETERS}
 )
 
 set(compiler_define ${compiler_define}
+    "USE_HAL_DRIVER"
     "STM32F429xx"
 )
--- getSourceList.sh
+++ getSourceList.sh
@@ -12,61 +12,61 @@ DRIVERS_PATH='Drivers/CMSIS/Device/*' # <----Changed path from Drivers/* which i
 # USB_PATH='USB_Device/*'
 
 # Clear the file
-> ./cmake/SourceList.cmake
+> cmake/SourceList.cmake
 
 # Print find result
 if [ -n "$APP_PATH" ]; then
 	APP_SRC="$(eval find "$APP_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
-	printf "$APP_SRC\n" >> ./cmake/SourceList.cmake
+	printf "$APP_SRC\n" >> cmake/SourceList.cmake
 fi
 
 if [ -n "$CORE_PATH" ]; then
 	CORE_SRC="$(eval find "$CORE_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
-	printf "$CORE_SRC\n" >> ./cmake/SourceList.cmake
+	printf "$CORE_SRC\n" >> cmake/SourceList.cmake
 fi
 
 if [ -n "$DRIVERS_PATH" ]; then
 	DRIVERS_SRC="$(eval find "$DRIVERS_PATH" -type f -name '*.c')"
-	printf "$DRIVERS_SRC\n" >> ./cmake/SourceList.cmake
+	printf "$DRIVERS_SRC\n" >> cmake/SourceList.cmake
 fi
 
 if [ -n "$FATFS_PATH" ]; then
 	FATFS_SRC="$(eval find "$FATFS_PATH" -type f -name '*.c')"
-	printf "$FATFS_SRC\n" >> ./cmake/SourceList.cmake
+	printf "$FATFS_SRC\n" >> cmake/SourceList.cmake
 fi
 
 if [ -n "$MIDWARE_PATH" ]; then
 	MIDWARE_SRC="$(eval find "$MIDWARE_PATH" -type f -name '*.c')"
-	printf "$MIDWARE_SRC\n" >> ./cmake/SourceList.cmake
+	printf "$MIDWARE_SRC\n" >> cmake/SourceList.cmake
 fi
 
 if [ -n "$USB_PATH" ]; then
 	USB_SRC="$(eval find "$USB_PATH" -type f -name '*.c')"
-	printf "$USB_SRC\n" >> ./cmake/SourceList.cmake
+	printf "$USB_SRC\n" >> cmake/SourceList.cmake
 fi
 
 # sed command works different for MacOS and Linux 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	# ========== FOR MACOS ==========
 	# Remove Empty Line
-	sed -i '' '/./!d' ./cmake/SourceList.cmake
+	sed -i '' '/./!d' cmake/SourceList.cmake
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i '' 's|^|${PROJ_PATH}/|' ./cmake/SourceList.cmake
+	sed -i '' 's|^|${PROJ_PATH}/|' cmake/SourceList.cmake
 
-	# Struct cmake format (Do not add tab space to line - 59)
+	# Struct cmake format (Do not add tab space to line - 58)
 	sed -i '' '1i\
-set(source_list ${source_list}' ./cmake/SourceList.cmake
-	sed -i '' -e '$a\'$'\n'')' ./cmake/SourceList.cmake
+set(source_list ${source_list}' cmake/SourceList.cmake
+	sed -i '' -e '$a\'$'\n'')' cmake/SourceList.cmake
 else
 	# ========== FOR LINUX  ==========
 	# Remove Empty Line
-	sed -i '/./!d' ./cmake/SourceList.cmake
+	sed -i '/./!d' cmake/SourceList.cmake
 
 	# Adding ${PROJ_PATH}/ to beginning of each line
-	sed -i 's|^|${PROJ_PATH}/|' ./cmake/SourceList.cmake
+	sed -i 's|^|${PROJ_PATH}/|' cmake/SourceList.cmake
 
 	# Struct cmake format
-	sed -i '1i set(source_list ${source_list}' ./cmake/SourceList.cmake
-	sed -i '$a)' ./cmake/SourceList.cmake
+	sed -i '1i set(source_list ${source_list}' cmake/SourceList.cmake
+	sed -i '$a)' cmake/SourceList.cmake
 fi
