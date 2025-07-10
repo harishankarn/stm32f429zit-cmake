4b6edd4793b51e0be0d31c3d4c5c4e733b33e61b
Hari Shankar <harishankarn04@gmail.com>
2025-07-10 11:07:56 +0530

    Stop tracking generated IncludeList.cmake

--- cmake/IncludeList.cmake
+++
@@ -1,7 +0,0 @@
-set(include_list ${include_list}
-${PROJ_PATH}/Application/Inc
-${PROJ_PATH}/Application/Inc
-${PROJ_PATH}/Core/Inc
-${PROJ_PATH}/Drivers/CMSIS/Include
-${PROJ_PATH}/Drivers/CMSIS/Device/ST/STM32F4xx/Include
-)
